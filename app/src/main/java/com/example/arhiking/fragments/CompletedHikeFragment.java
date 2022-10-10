e9324373b8030d6077dcf747e5e8c79d41b72cda
Romana <rfa008@post.uit.no>
2022-10-10 16:11:17 +0200

    UI - difficulty spinner

--- app/src/main/java/com/example/arhiking/fragments/CompletedHikeFragment.java
+++ app/src/main/java/com/example/arhiking/fragments/CompletedHikeFragment.java
@@ -9,8 +9,11 @@ import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.arhiking.MainActivity;
 import com.example.arhiking.R;
@@ -54,9 +57,30 @@ public class CompletedHikeFragment extends Fragment {
             }
         });
 
+        // Dropdown list for hike difficulty
+        Spinner difficultySpinner = root.findViewById(R.id.difficultySpinner);
+        // Array adapter with default layout and string array from resources
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.hike_difficulty_entries, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply adapter to the spinner
+        difficultySpinner.setAdapter(adapter);
+
         return root;
 
     }
+
+    //TODO : listener for hike difficulty dropdown
+
+    // Implement OnItemSelectedListener
+    /* public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }*/
 
     @Override
     public void onDestroyView() {
